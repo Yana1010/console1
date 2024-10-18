ad20cf8cc8f1dae4b58be9c678bd4955fa72a7b3
Yana1010 <yanalogan110@gmail.com>
2024-10-18 14:50:11 +0300

    Add files via upload

---
+++ ConsoleApplication5.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <string>
+#include <Windows.h>
+
+using namespace std;
+
+class ReportGenerator {
+public:
+    string generateReport(const string& data) {
+        // Генерація звіту на основі даних
+        return "Звіт: " + data;
+    }
+};
+
+class EmailSender {
+public:
+    void sendEmail(const string& report) {
+        // Відправка звіту електронною поштою
+        cout << "Відправка звіту: " << report << endl;
+    }
+};
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    string data = "Дані для звіту";
+
+    ReportGenerator reportGenerator;
+    string report = reportGenerator.generateReport(data);
+
+    EmailSender emailSender;
+    emailSender.sendEmail(report);
+
+    return 0;
+}
